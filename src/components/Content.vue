cb15a04613f044fbb25944ec72af448e51570ca9
pdvega <pdvegaa@gmail.com>
2017-08-11 16:37:21 +0700

    update content button

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,18 +2,19 @@
   <v-flex offset-xs3 xs12>
     <div class="row">
       <div class="col-md-8">
-        <p>6</p>
-        <p>Random Dice</p>
-        <v-btn fab dark large class="orange">
-          <v-icon dark>gamepad</v-icon>
-        </v-btn><br>
+        <p v-for="dice in dices">{{dice.dice}}
+          <v-btn fab dark large class="orange" @click="randomDadu(dice)" type="button" name="button">
+            <v-icon dark>gamepad</v-icon>
+          </v-btn>
+        </p>
+        <p>{{hasil}}</p>  
         <div>
-          <ul class="list-group">
-            <li class="list-group-item">
-              <span class="badge">2</span>
-              Player 1
-            </li>
-          </ul>
+          <v-list-tile>
+            <template v-for="todo in todos">
+              <v-list-tile-title>test</v-list-tile-title>
+              <v-list-tile-score>score</v-list-tile-score>
+            </template>
+          </v-list-tile>          
         </div>
       </div>
     </div>
@@ -22,6 +23,16 @@
 
 <script>
 export default {
+  data () {
+    return {
+      dices: [
+        { dice: 'test dice' }
+      ],
+      todos: [
+        { todo: 'test todo' }
+      ]
+    }
+  }
 }
 </script>
 
