538d1213dd66d4ffe940d7a5cb3f8a0c92d450fc
pdvega <pdvegaa@gmail.com>
2017-08-11 15:16:53 +0700

    update fitur content

---
+++ src/components/Content.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-flex offset-xs3 xs12>
+    <div class="row">
+      <div class="col-md-8">
+        <p>6</p>
+        <p>Random Dice</p>
+        <v-btn fab dark large class="orange">
+          <v-icon dark>gamepad</v-icon>
+        </v-btn><br>
+        <div>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <span class="badge">2</span>
+              Player 1
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </v-flex>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
