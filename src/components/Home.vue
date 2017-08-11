ff0831399b185aaf57eac46f1f8dbf06f2b9ee2c
Patrick Benz <patrickbenz.09@gmail.com>
2017-08-11 13:49:37 +0700

    Template

---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app id="application" standalone>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      light
+      overflow
+      absolute
+    >
+      <v-list class="pa-0">
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon dark @click.native.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="deep-orange" dark>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+    </v-toolbar>
+
+    <main>
+      <v-layout row wrap>
+        <Title></Title>     <!-- Title Component -->
+      </v-layout>
+    </main>
+
+  </v-app>
+</template>
+
+<script>
+import Title from '@/components/Title'
+export default {
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ],
+      mini: true,
+      right: null
+    }
+  },
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    },
+    { path: '/home', alias: '/', component: Home }
+  ]
+})
